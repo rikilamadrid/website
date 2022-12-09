40e2fe92cf6b3e98522d505702d3685b11ee0b52
Ricardo Lamadrid <ricardo.lamadrid@bldr.com>
2022-12-09 10:10:59 -0600

    updated color theme

--- script.js
+++ script.js
@@ -23,8 +23,8 @@ function setTheme(mode) {
   if (mode == 'blue') {
     document.getElementById('theme-style').href = 'blue.css';
   }
-  if (mode == 'green') {
-    document.getElementById('theme-style').href = 'green.css';
+  if (mode == 'neon') {
+    document.getElementById('theme-style').href = 'neon.css';
   }
   if (mode == 'purple') {
     document.getElementById('theme-style').href = 'purple.css';
